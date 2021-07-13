faf45983289b143c95462134aa13d522a6e5d6a4
Mateus Fernandes Pinto <mateus.pinto@bancointer.com.br>
2021-07-13 16:50:20 -0300

    mvvm ios acadamy

--- MVVMBindings/ViewController.swift
+++ MVVMBindings/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+//MARK: - Observable
 
+class Observable<T> {
+    var value: T? {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T?) {
+        self.value = value
+    }
+    
+    private var listener: ((T?) -> Void)?
+    
+    func bind(_ listener: @escaping (T?) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
+
+//MARK: - Model
+struct User: Decodable {
+    var name: String
+}
+
+//MARK: - ViewModels
+struct UserListViewModel {
+    var users: Observable<[UserTableViewCellViewModel]> = Observable([])
+}
+
+struct UserTableViewCellViewModel {
+    let name: String
+}
+
+//MARK: - View
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    private let viewModel = UserListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        
+        viewModel.users.bind { [weak self]_ in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        fetchData()
     }
-
-
+    
+    //MARK: - DataSourceTableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.users.value?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel.users.value?[indexPath.row].name
+        return cell
+    }
+    
+    func fetchData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let userModels = try JSONDecoder().decode([User].self, from: data)
+                
+                self.viewModel.users.value = userModels.compactMap({
+                    UserTableViewCellViewModel(name: $0.name)
+                })
+            }catch{
+                print("deu Ruim no request")
+            }
+        }
+        task.resume()
+    }
 }
 
