9e645ae2fdebc04b2797a476efeef58f01d856a2
Mateus Fernandes Pinto <mateus.pinto@bancointer.com.br>
2021-07-13 15:18:30 -0300

    Initial Commit

---
+++ MVVMBindings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBindings
+//
+//  Created by Mateus Fernandes Pinto on 13/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
